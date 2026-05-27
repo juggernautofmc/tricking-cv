2e37e3bd5be4107ee3703a7662fd12982de6c856
Eshwar Rajasekar <eshwar.rajasekar@gmail.com>
2026-05-27 15:46:25 -0700

    imma commit init

--- capture.py
+++ capture.py
@@ -5,3 +5,26 @@ import cv2
 # - read frames one at a time and return them to the main loop
 # - cleanly release the video source and close windows when done
 # - this file should NOT do any pose detection or drawing
+# - Eshwar
+
+
+class VideoCapture:
+
+    def __init__(self, source):
+        # input: source should be like a file path to example.mp4
+        # output: none lol, bust initialize self.cap
+        self.cap = cv2.VideoCapture(source)
+
+        
+
+
+    def read(self):
+        # input: none
+        # output: frame (numpy array) if successful, None if video is done or failed
+        pass
+
+    def release(self):
+        # input: none
+        # output: none, releases the video source and closes windows
+        pass
+
--- pose.py
+++ pose.py
@@ -6,3 +6,4 @@ import mediapipe as mp
 # - output a data object (you decide the shape) containing the 33 raw landmarks
 #   each landmark has x, y, z (depth) and visibility (confidence) fields
 # - return None (or similar) if no person is detected in the frame
+#-Eshwar 
