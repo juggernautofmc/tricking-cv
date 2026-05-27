e1f43ca2d94a850279add7ff27f1d67c0b737dc8
Eshwar Rajasekar <eshwar.rajasekar@gmail.com>
2026-05-27 16:24:47 -0700

    app.py order done

--- app.py
+++ app.py
@@ -13,3 +13,28 @@ import overlay
 #     3. compute metrics (analysis.py)
 #     4. draw and display (overlay.py)
 # - stop the loop when the video ends or the user quits
+
+
+def main(source):
+    # input: filepath 4 video, output: none
+    # opens video with capture.py, then loops through calling the other files until we done
+    cap = capture.VideoCapture(source)
+    pos = pose.PoseDetector()
+    anly = analysis.Analysis()
+
+    while True:
+        frame = cap.read()
+        if frame is None:
+            break
+        
+        landmarks = pos.process(frame)
+        if landmarks is None: #gotta check if there are no people in frame so we dont pass none
+            overlay.draw(frame, None, None) 
+        else:
+            metrics = anly.compute(landmarks)
+            overlay.draw(frame, landmarks, metrics)
+    cap.release()
+filepath = "backflippinggoonsesh.mp4"
+main(filepath)
+    
+
