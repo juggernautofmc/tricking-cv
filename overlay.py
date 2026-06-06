fbedb40879e237e2972145ec147f13807cc02ab4
Sami Alkharrat <alkharratsami@gmail.com>
2026-06-05 18:37:30 -0700

    added lines in overlay

--- overlay.py
+++ overlay.py
@@ -28,6 +28,8 @@ RED = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLUE = (255, 0, 0)
 YELLOW = (0, 255, 255)
+PINK = (193, 182, 255)
+
 
 
 class Overlay:
@@ -88,37 +90,74 @@ class Overlay:
 
     def draw_point(self, frame, x, y, color):
         cv2.circle(frame, (x, y), 10, color, -1)
+
+    def draw_line(self, frame, pt1, pt2, color):
+        cv2.line(frame, pt1, pt2, color, 2, cv2.LINE_AA)
     
     def draw_overlay(self, frame):
         # LEFT ARM
         if self.lv_shoulder > 0.5:
             self.draw_point(frame, self.lx_shoulder, self.ly_shoulder, RED)
         if self.lv_elbow > 0.5:
+            if self.lv_shoulder > 0.5:
+                self.draw_line(frame, (self.lx_shoulder, self.ly_shoulder), (self.lx_elbow, self.ly_elbow), RED)
             self.draw_point(frame, self.lx_elbow, self.ly_elbow, RED)
         if self.lv_wrist > 0.5:
+            if self.lv_elbow > 0.5:
+                self.draw_line(frame, (self.lx_elbow, self.ly_elbow), (self.lx_wrist, self.ly_wrist), RED)
             self.draw_point(frame, self.lx_wrist, self.ly_wrist, RED)
         
         # RIGHT ARM
         if self.rv_shoulder > 0.5:
             self.draw_point(frame, self.rx_shoulder, self.ry_shoulder, GREEN)
         if self.rv_elbow > 0.5:
+            if self.rv_shoulder > 0.5:
+                self.draw_line(frame, (self.rx_shoulder, self.ry_shoulder), (self.rx_elbow, self.ry_elbow), GREEN)
             self.draw_point(frame, self.rx_elbow, self.ry_elbow, GREEN)
         if self.rv_wrist > 0.5:
+            if self.rv_elbow > 0.5:
+                self.draw_line(frame, (self.rx_elbow, self.ry_elbow), (self.rx_wrist, self.ry_wrist), GREEN)
             self.draw_point(frame, self.rx_wrist, self.ry_wrist, GREEN)
 
         # LEFT LEG
         if self.lv_hip > 0.5:
             self.draw_point(frame, self.lx_hip, self.ly_hip, BLUE)
         if self.lv_knee > 0.5:
+            if self.lv_hip > 0.5:
+                self.draw_line(frame, (self.lx_hip, self.ly_hip), (self.lx_knee, self.ly_knee), BLUE)
             self.draw_point(frame, self.lx_knee, self.ly_knee, BLUE)
         if self.lv_heel > 0.5:
+            if self.lv_knee > 0.5:
+                self.draw_line(frame, (self.lx_knee, self.ly_knee), (self.lx_heel, self.ly_heel), BLUE)
             self.draw_point(frame, self.lx_heel, self.ly_heel, BLUE)
 
         # RIGHT LEG
         if self.rv_hip > 0.5:
             self.draw_point(frame, self.rx_hip, self.ry_hip, YELLOW)
         if self.rv_knee > 0.5:
+            if self.rv_hip > 0.5:
+                self.draw_line(frame, (self.rx_hip, self.ry_hip), (self.rx_knee, self.ry_knee), YELLOW)
             self.draw_point(frame, self.rx_knee, self.ry_knee, YELLOW)
         if self.rv_heel > 0.5:
+            if self.rv_knee > 0.5:
+                self.draw_line(frame, (self.rx_knee, self.ry_knee), (self.rx_heel, self.ry_heel), YELLOW)
             self.draw_point(frame, self.rx_heel, self.ry_heel, YELLOW)
+        
+        # MIDPOINTS
+        hips_midpt_x = (self.rx_hip + self.lx_hip) // 2
+        hips_midpt_y = (self.ry_hip + self.ly_hip) // 2
+        shoulders_midpt_x = (self.rx_shoulder + self.lx_shoulder) // 2
+        shoulders_midpt_y = (self.ry_shoulder + self.ly_shoulder) // 2
+
+        if self.rv_shoulder > 0.5 and self.lv_shoulder > 0.5:
+            self.draw_point(frame, shoulders_midpt_x, shoulders_midpt_y, PINK)
+            self.draw_line(frame, (self.rx_shoulder, self.ry_shoulder), (shoulders_midpt_x, shoulders_midpt_y), PINK)
+            self.draw_line(frame, (self.lx_shoulder, self.ly_shoulder), (shoulders_midpt_x, shoulders_midpt_y), PINK)
+            if self.rv_hip > 0.5 and self.lv_hip > 0.5:
+                self.draw_line(frame, (shoulders_midpt_x, shoulders_midpt_y), (hips_midpt_x, hips_midpt_y), PINK)
+        
+        if self.rv_hip > 0.5 and self.lv_hip > 0.5:
+            self.draw_point(frame, hips_midpt_x, hips_midpt_y, PINK)
+            self.draw_line(frame, (self.rx_hip, self.ry_hip), (hips_midpt_x, hips_midpt_y), PINK)
+            self.draw_line(frame, (self.lx_hip, self.ly_hip), (hips_midpt_x, hips_midpt_y), PINK)
         
